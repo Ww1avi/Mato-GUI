7c5c29f8745e50a410f4b82c793bab33686034ff
Ww1avi <kandonmezmomo@gmail.com>
2025-02-08 15:13:23 +0100

    Silly Seals.lua

--- Silly Seals.lua
+++ Silly Seals.lua
@@ -5,7 +5,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Mato Gui",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
+   LoadingTitle = "Silly Seal Script",
    LoadingSubtitle = "by Mato",
    Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
